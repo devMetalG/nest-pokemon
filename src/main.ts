786c7766db3e1b06cf5ed9d8d881c4f02b5e04c3
Guillermo Franco <fra_borjon@hotmail.com>
2026-01-19 17:10:15 -0600

    feat: Bind application to all network interfaces by listening on 0.0.0.0.

--- src/main.ts
+++ src/main.ts
@@ -16,7 +16,7 @@ async function bootstrap() {
   );
   app.setGlobalPrefix('api/v2');
   const port = process.env.PORT ?? 3000;
-  await app.listen(port);
+  await app.listen(port, '0.0.0.0');
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
