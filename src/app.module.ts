945f80b8e8ead93453102ea967b86ba8953d8d6a
Guillermo Franco <fra_borjon@hotmail.com>
2026-01-19 13:59:17 -0600

    refactor: Migrate ServeStaticModule to async configuration, remove MongooseModule, and comment out port in app.config.

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,9 @@ import { join } from 'path';
 import { Module } from '@nestjs/common';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { PokemonModule } from './pokemon/pokemon.module';
-import { MongooseModule } from '@nestjs/mongoose';
 import { CommonModule } from './common/common.module';
 import { SeedModule } from './seed/seed.module';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { appConfig } from './config/app.config';
 import { JoiValidationSchema } from './config/joi.validation';
 
@@ -15,11 +14,17 @@ import { JoiValidationSchema } from './config/joi.validation';
       load: [appConfig],
       validationSchema: JoiValidationSchema,
     }),
-    ServeStaticModule.forRoot({
-      rootPath: join(__dirname, '..', 'public'),
-    }),
-    MongooseModule.forRoot(process.env.MONGODB || '', {
-      dbName: 'pokemon',
+    ServeStaticModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        return [
+          {
+            uri: configService.get<string>('mongodb'),
+            rootPath: join(__dirname, '..', 'public'),
+          },
+        ];
+      },
     }),
     PokemonModule,
     CommonModule,
